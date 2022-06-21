47b05573cb3ae898ee9b829650a8ed70a495b5da
Vincent Sjögren <vincentsjogren@gmail.com>
2022-06-21 18:43:54 +0200

    removed recoil

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
 import SocketProvider from './context/SocketProvider';
 import App from './App';
 import './index.css';
@@ -8,9 +7,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<SocketProvider>
-			<RecoilRoot>
-				<App />
-			</RecoilRoot>
+			<App />
 		</SocketProvider>
 	</React.StrictMode>
 );
